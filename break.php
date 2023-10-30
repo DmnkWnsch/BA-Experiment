b12cd2107d666433baa4f09ea1d0a1dd0a14346e
DmnkWnsch <wuensch_dominik@t-online.de>
2023-10-30 14:45:33 +0100

    Feat: Create style for index, break and end page

--- break.php
+++ break.php
@@ -1,5 +1,5 @@
 <?php
-require ($_SERVER["DOCUMENT_ROOT"] . '/util/util.php');
+/*require ($_SERVER["DOCUMENT_ROOT"] . '/util/util.php');
 $util = new Util();
 session_start();
 
@@ -19,10 +19,64 @@ if ($_SESSION['saved'] == false) {
     $_SESSION['current_site'] = $_SESSION['current_site'] + 1;
     $_SESSION['saved'] = true;
     $util->updateUID($_SESSION['uid'], $_SESSION['current_banner'], $_SESSION['current_site']);
-}
+}*/
 
-var_dump($_SESSION);
+//$newSite = $_SESSION['$current_site'];
+$newSite = 2;
+$shownSite = $newSite - 1;
+$progress = (100 / 4) * ($shownSite);
 
 ?>
 
-<a href="show.php">WEITER<a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BA-Experiment</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container-md mt-5">
+      <div class="row justify-content-center">
+        <div class="col-12 col-xl-10 col-xxl-9">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title text-center mb-3">
+                Experiment zur Positionierung des Impressums
+              </h3>
+              <div class="progress mb-3" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar text-light w-<?= $progress ?>"><?= $progress ?>%</div>
+              </div>
+              <p class="card-text px-1">
+                Die Bearbeitung von <b>Seite <?= $shownSite ?></b> ist damit abgeschlossen.<br>
+                Weiter geht es mit einem Klick auf den Button.
+              </p>
+
+              <div class="row mt-4">
+                <div class="d-grid col-6 mx-auto">
+                  <a
+                    href="show.php"
+                    class="btn btn-success btn-fluid"
+                    >Weiter zu Seite <?= $newSite ?></a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
--- thanks.php
+++ thanks.php
@@ -1,16 +1,79 @@
 <?php
+require ($_SERVER["DOCUMENT_ROOT"] . '/util/util.php');
+$util = new Util();
 session_start();
 
-if (!isset($_SESSION['uid'])) {
+/*if (!isset($_SESSION['uid'])) {
     echo "Es ist ein Fehler aufgetreten. Bitte starte das Expirment erneut.";
     die();
 }
 
-$uid = $_SESSION['uid'];
+# As we have 4 webpages to show, redirect to the ending page when we get here and have 4 pages visited
+if ($_SESSION['current_site'] < 4) {
+    header("Location: break.php");
+    die();
+}*/
+
+//$uid = $_SESSION['uid'];
+$uid = "c4ad7a66-d847-4abc-a67a-d582854dea0e";
+//$util->setUIDFinished($uid);
+
+$poll_link = "https://bildungsportal.sachsen.de/umfragen/limesurvey/index.php/238957?user_id=$uid"
 
 # TODO Link zur Weiterleitung zu LimeSurvey --> uid an Limesurvey weitergeben um beide Seiten miteinander in Verbindung bringen zu können
 # Example: https://bildungsportal.sachsen.de/umfragen/limesurvey/index.php/238957?user_id=c4ad7a66-d847-4abc-a67a-d582854dea0e
 
 ?>
 
-Danke für deine Teilnahme.
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BA-Experiment</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container-md mt-5">
+      <div class="row justify-content-center">
+        <div class="col-12 col-xl-10 col-xxl-9">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title text-center mb-3">
+                Experiment zur Positionierung des Impressums
+              </h3>
+              <div class="alert alert-success border-0" role="alert">
+                Du hast den <b>ersten Teil</b> des Experiments abgeschlossen!
+              </div>
+              <p class="card-text px-1">
+                Um die Bearbeitung forzusetzen und mit dem zweiten Teil zu beginnen, drücken Sie
+                bitte den Button.
+              </p>
+
+              <div class="row mt-4">
+                <div class="d-grid col-6 mx-auto">
+                  <a
+                    href="<?= $poll_link ?>"
+                    class="btn btn-primary"
+                    >Weiter zur Umfrage</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
